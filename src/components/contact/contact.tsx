7c9df2b3e0aaeb987334db0a8a91c0efe1375148
Gowthaman Ravindrathas <gowtham4n@gmail.com>
2023-05-30 02:51:57 +0100

    Compress assets

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -20,7 +20,7 @@ export default function Contact() {
           Get in touch
           <FontAwesomeIcon
             icon={faArrowRight}
-            className="inline h-[16px] hover:text-[var(--green-bright)] px-2 duration-150 group-hover:translate-x-1"
+            className="inline h-[16px] hover:text-[var(--green-bright)] px-2 translate-y-[3px] duration-150 group-hover:translate-x-1"
             size="xs"
           />
         </a>
